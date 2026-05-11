2969bdaea6efd4d3fb4cf651d64ab565fae04c30
Daniele Gazzelloni <dangz.dev@gmail.com>
2026-05-11 12:37:32 +0100

    chore: rename users-list feature folder to match feature name

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { onMounted } from 'vue'
 import { default as ChatLayout} from '@/components/layout/Default.vue'
 import LoginView from '@/features/login/LoginView.vue'
-import UsersList from '@/features/sidebar/UsersList.vue'
+import UsersList from '@/features/users-list/UsersList.vue'
 import Chat from '@/features/chat/Chat.vue'
 import useChatSocket from '@/features/chat/composables/useChatSocket'
 
